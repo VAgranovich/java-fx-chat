630ef0b6cc726be5c1409007d7980172fa04f9cb
Vagranovich <vadim.agranovich@gmail.com>
2022-07-04 01:15:47 +0300

    Final commit

--- src/main/java/ru/gb/javafxchat/client/ChatController.java
+++ src/main/java/ru/gb/javafxchat/client/ChatController.java
@@ -1,1 +1,89 @@
 package ru.gb.javafxchat.client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+import java.util.Optional;
+
+public class ChatController {
+    @FXML
+    private TextField loginField;
+    @FXML
+    private HBox authBox;
+    @FXML
+    private PasswordField passField;
+    @FXML
+    private VBox messageBox;
+
+    @FXML
+    private TextArea messageArea;
+    @FXML
+    private TextField messageField;
+
+    private final ChatClient client;
+
+    public ChatController() {
+        this.client = new ChatClient(this);
+        while (true) {
+
+            try {
+                client.openConnection();
+                break;
+            } catch (IOException e) {
+                showNotification();
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void showNotification() {
+        Alert alert = new Alert(Alert.AlertType.ERROR,
+                "Не могу подключиться к серверу. \n" + "Проверьте, что сервер запущен",
+                new ButtonType("Попробовать снова", ButtonBar.ButtonData.OK_DONE),
+                new ButtonType("Выйти", ButtonBar.ButtonData.CANCEL_CLOSE)
+        );
+        alert.setTitle("Ошибка подключения");
+
+        Optional<ButtonType> answer = alert.showAndWait();
+        Boolean isExit = answer.map(select -> select.getButtonData().isCancelButton()).orElse(false);
+        if (isExit) {
+            System.exit(0);
+        }
+    }
+
+    public void clickNewChat(ActionEvent actionEvent) {
+    }
+
+    public void clickExit(ActionEvent actionEvent) {
+    }
+
+    public void clickSendButton() {
+
+        String message = messageField.getText();
+        if (message.isBlank()) return;
+
+        //messageArea.appendText(message + "\n");
+        client.sendMessage(message);
+        messageField.clear();
+        messageField.requestFocus();
+    }
+
+    public void addMessage(String message) {
+        messageArea.appendText(message + "\n");
+
+    }
+
+    public void setAuth(boolean success) {
+        authBox.setVisible(!success);
+        messageBox.setVisible(success);
+    }
+
+    public void signingBtnClick() {
+        client.sendMessage("/auth " + loginField.getText() + " " + passField.getText());
+    }
+}
